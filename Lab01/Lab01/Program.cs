aef57edbd26ba3ee692ce9e6bb4fc2820a952410
wwiderski <chx79073@student.chorzow.wsb.pl>
2023-03-18 13:44:27 +0100

    test2

--- Lab01/Lab01/Program.cs
+++ Lab01/Lab01/Program.cs
@@ -1,3 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!!");
+// sprawdzenie synchronizacji 
 Console.WriteLine("test");
+Console.WriteLine("test2");
