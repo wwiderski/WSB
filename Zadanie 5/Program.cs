57407a3e5d97a09b96bc0e52f69d36308551b29b
wwiderski <chx79073@student.chorzow.wsb.pl>
2023-06-03 19:22:16 +0200

    Zadanie 5

---
+++ Zadanie 5/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b;
+
+        Console.WriteLine("podaj a");
+        a = inputValue();
+        Console.WriteLine("Podaj b");
+        b = inputValue();
+        if (a < b)
+        {
+            Console.WriteLine("Wieksza jest liczba b:");
+            Console.WriteLine(b);
+        }
+        else
+        {
+            Console.WriteLine("Wieksza jest liczba a:");
+            Console.WriteLine(a);
+        }
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
