9e51f2eb582ddb3908b8f06f3db0214615550c0c
wwiderski <chx79073@student.chorzow.wsb.pl>
2023-06-03 16:01:52 +0200

    Zadanie 2

--- Zadani 2/Program.cs
+++ Zadani 2/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double o, p, r;
+        var pi = Math.PI;
+        Console.WriteLine("podaj pole kola:");
+        p = inputValue();
+        r = Math.Sqrt(p / pi);
+        o = 2 * pi * r;
+        Console.WriteLine("obwod kola wynosi:");
+        Console.WriteLine(o);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
+
