4fec5f1744775656a074cb9677a610f3890aec9b
wwiderski <chx79073@student.chorzow.wsb.pl>
2023-06-03 15:25:34 +0200

    Create Zadania 1.cs

---
+++ Zadania 1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c;
+
+        Console.WriteLine("podaj wartosc a:");
+        a = inputValue();
+        Console.WriteLine("podaj wartosc b:");
+        b = inputValue();
+        Console.WriteLine("podaj wartosc c:");
+        c = inputValue();
+        double sredniArytmetyczna;
+
+        sredniArytmetyczna = (a + b + c) / 3;
+        Console.WriteLine("Srednia wynosi");
+        Console.WriteLine(sredniArytmetyczna);
+    }
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
