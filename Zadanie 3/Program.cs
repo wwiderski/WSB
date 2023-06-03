7279b763ca51afc43112b5b52eec420384903cfd
wwiderski <chx79073@student.chorzow.wsb.pl>
2023-06-03 17:44:58 +0200

    Zadanie 3

---
+++ Zadanie 3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double w, h, bmi;
+        Console.WriteLine("podaj wage:");
+        w = inputValue();
+        Console.WriteLine("podaj wzrost");
+        h = inputValue();
+        bmi = w / (h * h);
+        Console.WriteLine("BMI wynosi");
+        Console.WriteLine(bmi);
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
