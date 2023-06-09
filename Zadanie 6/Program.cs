6d51b40e75645a0f1e4d6a41e900ede81cb186bf
wwiderski <chx79073@student.chorzow.wsb.pl>
2023-06-09 09:52:13 +0200

    Zadanie 6 v2

---
+++ Zadanie 6/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class MyProgram
+{
+    public static void Main(string[] args)
+    {
+        double a, b, c;
+
+        Console.WriteLine("podaj bok A");
+        a = inputValue();
+        Console.WriteLine("podaj bok B");
+        b = inputValue();
+        Console.WriteLine("podaj bok c");
+        c = inputValue();
+        if (a == b || a == c || b == c)
+        {
+            Console.WriteLine("trójkąt jest równoboczny");
+        }
+        else
+        {
+            Console.WriteLine("trójkąt nie jest równoboczny");
+        }
+    }
+
+    // .NET can only read single characters or entire lines from the
+    // console. The following function safely reads a double value.
+    private static double inputValue()
+    {
+        double result;
+        while (!double.TryParse(Console.ReadLine(), out result)) ;
+        return result;
+    }
+}
