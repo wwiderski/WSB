63b1dd225830d2c5c6cf643ac4458073e1793baa
wwiderski <chx79073@student.chorzow.wsb.pl>
2023-06-09 11:01:59 +0200

    Zadanie 7

---
+++ Zadanie 7/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Podaj współrzędną x punktu P:");
+        double x = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj współrzędną y punktu P:");
+        double y = Convert.ToDouble(Console.ReadLine());
+
+        Console.WriteLine("Podaj promień koła:");
+        double r = Convert.ToDouble(Console.ReadLine());
+
+        double distance = Math.Sqrt(x * x + y * y);
+
+        if (distance <= r)
+        {
+            Console.WriteLine("Punkt leży w obrębie koła o promieniu.", x, y, r);
+        }
+        else
+        {
+            Console.WriteLine("Punkt nie leży w obrębie koła o promieniu.", x, y, r);
+        }
+    }
+}
