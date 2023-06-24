bbfa599584fd8f0ed8e898fcc113cd3d69de12cc
wwiderski <chx79073@student.chorzow.wsb.pl>
2023-06-24 03:49:45 +0200

    Ćwiczenie 4 v2

---
+++ Ćwiczenie 4/Zadanie 1/tablice.cs
@@ -0,0 +1,76 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int rozmiar_tablicy = 10;
+        int[] tablica = new int[rozmiar_tablicy];
+        Random random = new Random();
+
+        // Wypełnienie tablicy losowymi wartościami
+        for (int i = 0; i < rozmiar_tablicy; i++)
+        {
+            tablica[i] = random.Next(100); // zakres losowych wartości od 0 do 99
+        }
+
+        // Wypisanie zawartości tablicy od tyłu
+        for (int i = rozmiar_tablicy - 1; i >= 0; i--)
+        {
+            Console.WriteLine(tablica[i]);
+        }
+
+
+        int najmniejszy = tablica[0];
+        int największy = tablica[0];
+
+        for (int i = 1; i < tablica.Length; i++)
+        {
+            if (tablica[i] < najmniejszy)
+            {
+                najmniejszy = tablica[i];
+            }
+
+            if (tablica[i] > największy)
+            {
+                największy = tablica[i];
+            }
+        }
+
+        Console.WriteLine("Najmniejszy element: " + najmniejszy);
+        Console.WriteLine("Największy element: " + największy);
+
+
+
+
+        int n = tablica.Length;
+        bool zamiana;
+        do
+        {
+            zamiana = false;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (tablica[i] > tablica[i + 1])
+                {
+                    int temp = tablica[i];
+                    tablica[i] = tablica[i + 1];
+                    tablica[i + 1] = temp;
+                    zamiana = true;
+                }
+            }
+
+            n--;
+        } while (zamiana);
+
+        Console.WriteLine("Posortowana tablica:");
+
+        for (int i = 0; i < tablica.Length; i++)
+        {
+            Console.WriteLine(tablica[i]);
+        }
+    }
+
+
+}
+
